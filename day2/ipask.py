bc8a274fff21f18ce54ad24750b146c4368ee36b
Eddie Lerma <Heriberto.Lerma@outlook.com>
2018-05-22 12:15:22 -0400

    day 2 morning labs

---
+++ day2/ipask.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# code is to display sentence containing IP address and Mac Address
+
+ip = input('Please enter IP address:')
+print('Tell me and IP address:'+ str(ip))
+
+mac = input('Please enter MAC address:')
+print('Tell me a MAC address:'+ str(mac))
+
---
+++ day2/orange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# To make ORANGE be displayed using six print functions.
+
+print('O', end='')
+print('R', end='') 
+print('A', end='') 
+print('N', end='') 
+print('G', end='') 
+print('E')
