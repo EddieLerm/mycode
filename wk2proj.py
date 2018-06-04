a4df789c5b0a85ee71c4b43efc43839e1310321e
Eddie Lerma <Heriberto.Lerma@outlook.com>
2018-06-04 12:43:24 -0400

    corrections

--- wk2proj.py
+++ wk2proj.py
@@ -2,13 +2,13 @@
 
 def shorun_sw():
 
-    from napalm import get_network_driver
+    from napalm import get_network_driver   # Import driver information
 
     driver = get_network_driver('eos')
 
-    sw = input('Enter desired sw number: ')
+    sw = input('Enter desired sw number: ')    # Input to specifiy desired Switch
 
-    if sw == '1' :
+    if sw == '1' :    # information for Switch 1
     
         sw1 = driver('172.16.2.10', 'admin', 'alta3')
 
@@ -16,7 +16,7 @@ def shorun_sw():
 
         print(sw1.get_config())
 
-    elif sw == '2' :
+    elif sw == '2' :    # information for Switch 2
 
         sw2 = driver('172.16.2.20', 'admin', 'alta3')
 
@@ -24,7 +24,7 @@ def shorun_sw():
 
         print(sw2.get_config())
 
-    else:
+    else:    # If non specified input is entered
 
         print('Please enter valid sw number')
 
